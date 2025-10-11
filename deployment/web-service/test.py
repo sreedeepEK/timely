7600be8ba969701448833b653352904d0ddf5e7f
Sreedeep . <sreedeepek.95@gmail.com>
2025-10-12 00:39:39 +0530

    add flask app

--- deployment/web-service/test.py
+++ deployment/web-service/test.py
@@ -1,13 +1,14 @@
-import predict
-
+import requests
 
 ride = {
-    "PULocationID": 10, 
-    "DOLocationID": 35,
-    "trip_distance": 40 
-    }
+    "PULocationID": 10,
+    "DOLocationID": 50,
+    "trip_distance": 40
+}
+
+url = 'http://localhost:9696/predict'
+response = requests.post(url, json=ride)
+print(response.json())
 
 
-pred = predict.predict(ride)
-
-print(pred)
+# next - add gunicorn server for productionizing. 
